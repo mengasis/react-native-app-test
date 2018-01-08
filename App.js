21637ae51865d7e25c92f962238d4726ed96589b
Manuel Venegas <mengasis@outlook.com>
2018-01-07 23:47:34 -0300

    Added Box

--- App.js
+++ App.js
@@ -1,23 +1,70 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		const title = 'Batalla de Gránico'
+		const urlImage =
+			'https://assets.arrecaballo.es/wp-content/uploads/2014/03/batalla-de-granico-alejandro-cruzando-el-rio.png'
+
+		return (
+			<View style={styles.container}>
+				<View style={styles.box}>
+					<Image style={styles.image} source={{ uri: urlImage }} />
+					<View style={styles.info}>
+						<Text style={styles.title}>{title}</Text>
+						<View style={styles.actions}>
+							<View style={styles.icons}>
+								<Icon name="heart-o" size={30} color="white" />
+								<Text style={styles.text}>50</Text>
+							</View>
+							<View style={styles.icons}>
+								<Icon name="comments" size={30} color="white" />
+								<Text style={styles.text}>50</Text>
+							</View>
+						</View>
+					</View>
+				</View>
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#F1E57A',
+		paddingTop: 50
+	},
+	box: {
+		flexDirection: 'row',
+		backgroundColor: '#FFF',
+		margin: 5
+	},
+	image: {
+		width: 150,
+		height: 150
+	},
+	info: {
+		flex: 1,
+		backgroundColor: 'black',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	actions: {
+		flexDirection: 'row'
+	},
+	icons: {
+		flex: 1,
+		alignItems: 'center'
+	},
+	title: {
+		fontSize: 18,
+		color: 'white'
+	},
+	text: {
+		color: 'white'
+	}
+})
