5b15c67c05e7e9e7a16a13351523dc8220ba6753
Manuel Venegas <mengasis@outlook.com>
2018-01-08 00:29:41 -0300

    The battles of Alexander the Great added as items

--- App.js
+++ App.js
@@ -2,28 +2,10 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import Box from './Box'
+import data from './battles'
 
 export default class App extends React.Component {
 	render() {
-		const data = [
-			{
-				id: 1,
-				title: 'Batalla de Gránico',
-				image:
-					'https://assets.arrecaballo.es/wp-content/uploads/2014/03/batalla-de-granico-alejandro-cruzando-el-rio.png',
-				likes: 50,
-				comments: 20
-			},
-			{
-				id: 2,
-				title: 'Batalla de Gaugamela',
-				image:
-					'http://4.bp.blogspot.com/-NHmZKIKL9js/UUm-pamwzNI/AAAAAAAADwM/VVRv4OX8-Pw/s1600/batalla-de-gaugamela.JPG',
-				likes: 112,
-				comments: 45
-			}
-		]
-
 		return (
 			<View style={styles.container}>
 				{data.map(item => <Box key={item.id} {...item} />)}
---
+++ battles.js
@@ -0,0 +1,42 @@
+export default [
+	{
+		id: 1,
+		title: 'Batalla de Gránico',
+		image:
+			'https://assets.arrecaballo.es/wp-content/uploads/2014/03/batalla-de-granico-alejandro-cruzando-el-rio.png',
+		likes: 50,
+		comments: 20
+	},
+	{
+		id: 2,
+		title: 'Batalla de Gaugamela',
+		image:
+			'http://4.bp.blogspot.com/-NHmZKIKL9js/UUm-pamwzNI/AAAAAAAADwM/VVRv4OX8-Pw/s1600/batalla-de-gaugamela.JPG',
+		likes: 112,
+		comments: 45
+	},
+	{
+		id: 3,
+		title: 'Batalla de Hidaspes',
+		image:
+			'https://assets.arrecaballo.es/wp-content/uploads/2014/04/batalla-de-hidaspes-326-ac-contra-los-elefantes-hindues.png',
+		likes: 86,
+		comments: 34
+	},
+	{
+		id: 4,
+		title: 'Batalla de Issos',
+		image:
+			'https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Darius_III.jpg/800px-Darius_III.jpg',
+		likes: 108,
+		comments: 21
+	},
+	{
+		id: 5,
+		title: 'Batalla de Queronea',
+		image:
+			'http://2.bp.blogspot.com/-R0AkKdP8qJg/Vpfn64BAvEI/AAAAAAAAGRo/sfMrIhYJgAc/s1600/batalla%2Bde%2BQueronea.jpg',
+		likes: 65,
+		comments: 16
+	}
+]
